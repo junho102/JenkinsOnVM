262ea804d47cad840950242da00a12356633b847
junoho102 <krle102@naver.com>
2021-07-15 15:12:12 +0900

    test4rd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like IU(test3)');
+    res.send('I like IU(test4)');
 });
 
 app.get('/start', function(req, res){
