1eabbff87e6d5d28353fef01cb62861c2c4c2a1e
junho102 <krle102@naver.com>
2021-07-15 22:28:23 +0900

    8th_0715

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like IU & girlFriend ???????');
+    res.send('I like IU & WonderGirls');
 });
 
 app.get('/start', function(req, res){
